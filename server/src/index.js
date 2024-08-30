257d1f70d2c511e70b7619c52fd13a3ce4c59366
Umesh Nagare <umeshnagare27@gmail.com>
2024-08-30 13:47:09 +0530

    server and cors init

---
+++ server/src/index.js
@@ -0,0 +1,20 @@
+import corsMiddleware from "./middlewares/cors.js";
+import { config as configDotenv } from "dotenv";
+import { app } from "./app.js";
+configDotenv();
+
+// constants
+const PORT = process.env.PORT || 3000;
+
+// middlewares
+app.use(corsMiddleware);
+
+
+// routes
+app.get("/", (req, res) => {
+  res.send("Welcome To KrishiNET");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
