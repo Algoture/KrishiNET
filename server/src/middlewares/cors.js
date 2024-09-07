166e76dc7f0c1e9423f9d3770a0b11a60e7f05d9
Umesh Nagare <umeshnagare27@gmail.com>
2024-09-07 16:30:25 +0530

    Tech Stack Change

--- server/src/middlewares/cors.js
+++
@@ -1,8 +0,0 @@
-import cors from "cors";
-const corsOptions = {
-  origin: "http://localhost:5173/",
-  optionsSuccessStatus: 200,
-};
-
-const corsMiddleware = cors(corsOptions);
-export default corsMiddleware;
