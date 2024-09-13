9ecac67929f6a70f35c3bd1de429e608032e695d
Umesh Nagare <umeshnagare27@gmail.com>
2024-09-13 23:09:30 +0530

    IP address Location

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,32 @@
 import { NavLink } from "react-router-dom";
-
+import ipapi from "ipapi.co";
+import { useState, useEffect } from "react";
 const HomePage = () => {
+  const [ip, setIp] = useState("");
+  const [location, setLocation] = useState("");
+  const [region, setRegion] = useState("");
+
+  const fetchIp = async () => {
+    try {
+      const response = await fetch("https://api.ipify.org?format=json");
+      const data = await response.json();
+      setIp(data.ip);
+      const locationResponse = await fetch(`https://ipapi.co/${data.ip}/json/`);
+      const locationData = await locationResponse.json();
+      setLocation(locationData.city);
+      setRegion(locationData.region);
+      console.log(locationData);
+    } catch (error) {
+      console.error("Error fetching IP or location:", error);
+    }
+  };
+  useEffect(() => {
+    fetchIp();
+  }, []);
   return (
     <div className="flex flex-col items-center mt-10">
-      <h1 className="text-3xl font-bold">Welcome to KrishiNET</h1>
+      <h1 className="text-3xl font-bold">Welcome to KrishiNET {location}</h1>
+      <h1 className="text-3xl font-bold">Jai {region} ! From API</h1>
       <div className="flex gap-4 mt-4">
         <NavLink
           to="/auth"
