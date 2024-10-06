322e293c53918ccf3424f14a8a208fa52af7c909
Umesh Nagare <umeshnagare27@gmail.com>
2024-10-06 19:57:50 +0530

    Width problem fixed

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -73,7 +73,7 @@ export default function LandingPage() {
 
           <div className="relative max-lg:row-start-1">
             <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-t-[2rem]"></div>
-            <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)]">
+            <div className="relative flex  flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)]">
               <div className="px-8 pt-8 sm:px-10 sm:pt-10">
                 <p className="mt-2 text-lg/7 font-medium tracking-tight text-gray-950 max-lg:text-center">
                   Local Sourcing
@@ -84,7 +84,7 @@ export default function LandingPage() {
                 </p>
               </div>
               <div className="flex flex-1 items-center justify-center px-8 max-lg:pb-12 max-lg:pt-10 sm:px-10 lg:pb-2">
-                <iframe src="https://lottie.host/embed/f6b3d945-d207-4c63-88f0-ffd1c26c64bb/5DS0RUv5yi.json"></iframe>
+                <iframe className="w-full " src="https://lottie.host/embed/f6b3d945-d207-4c63-88f0-ffd1c26c64bb/5DS0RUv5yi.json"></iframe>
               </div>
             </div>
             <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-accent max-lg:rounded-t-[2rem]"></div>
