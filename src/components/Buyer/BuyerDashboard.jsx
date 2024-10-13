404a98460dc03cfe7224e80969e8f0d286055755
Sainath0009 <sainathmedpalli@.com>
2024-10-13 10:31:46 +0530

    new category section

--- src/components/Buyer/BuyerDashboard.jsx
+++ src/components/Buyer/BuyerDashboard.jsx
@@ -1,9 +1,20 @@
 import cropData from "../../utils/Data";
 import SearchBar from "../SearchBar";
 import CropCard from "./CropCard";
+
 const BuyerDashboard = ({ user }) => {
+  const categories = [
+    { name: "Fruits", src: "https://res.cloudinary.com/dvytn4u6i/image/upload/v1710678972/003_orange_984217ca48.png" },
+    { name: "Grains", src: "https://res.cloudinary.com/dvytn4u6i/image/upload/v1710678972/006_wheat_sack_80f6f0f6db.png" },
+    { name: "Herbs", src: "https://www.rd.com/wp-content/uploads/sites/2/2016/06/09-medicinal-herbs-you-can-grow-parsley.jpg" },
+    { name: "Veggies", src: "https://res.cloudinary.com/dvytn4u6i/image/upload/v1710678972/004_vegetables_5494192878.png" },
+    { name: "Dairy", src: "https://res.cloudinary.com/dvytn4u6i/image/upload/v1710678972/005_milk_d2274408c0.png" },
+    { name: "Spices", src: "https://png.pngtree.com/png-vector/20240202/ourmid/pngtree-pieces-of-cooking-spices-png-png-image_11591592.png" },
+    { name: "lentils", src: "https://png.pngtree.com/png-vector/20231101/ourmid/pngtree-red-lentils-in-a-bowl-from-above-protein-png-image_10389107.png" }
+  ];
+
   return (
-    <div className="bg-primary text-black w-full ">
+    <div className="bg-primary text-black w-full">
       <div className="p-4 w-full">
         <div className="flex items-center mb-8 justify-between">
           <h1 className="text-3xl font-bold">
@@ -13,23 +24,27 @@ const BuyerDashboard = ({ user }) => {
             <SearchBar />
           </div>
         </div>
-        <div className="mb-4">
-          <h2 className="text-xl  font-semibold">Categories</h2>
-          {/* <div className="flex space-x-4 mt-2">
-            <button className=" bg-green-300 rounded-full px-4 py-2 flex items-center space-x-2">
-              <img
-                alt="Fruits"
-                className="rounded-full object-cover"
-                width="50"
-                height="50"
-                src="https://th.bing.com/th/id/R.e664b16f26647216cf8727f375b02af6?rik=WNhj5SbsXtnbwA&riu=http%3a%2f%2fupload.wikimedia.org%2fwikipedia%2fcommons%2f2%2f2f%2fCulinary_fruits_front_view.jpg&ehk=sr%2ffErwRNU43LX0Q2RKHZov%2fci7QtR2%2fH0MZsT6o%2fC4%3d&risl=1&pid=ImgRaw&r=0"
-              />
-              <span>Fruits</span>
-            </button>
-          </div> */}
+
+        <div className='mt-10'>
+          <h2 className='text-green-600 font-bold text-2xl'>Shop by Category</h2>
+          <div className='grid grid-cols-3 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-7 gap-5 mt-2'>
+            {categories.map((category, index) => (
+              <div key={index} className='flex flex-col items-center bg-green-50 gap-2 p-3 rounded-lg group cursor-pointer hover:bg-accent'>
+                <img
+                  alt={category.name}
+                  width="50"
+                  height="50"
+                  src={category.src}
+                  className='group-hover:scale-125 transition-all ease-in-out'
+                />
+                <h2 className='text-green-800 group-hover:text-white'>{category.name}</h2>
+              </div>
+            ))}
+          </div>
         </div>
+
         <div className="w-full">
-          <h2 className="text-xl  font-semibold">Browse Products</h2>
+          <h2 className="text-xl font-semibold">Browse Products</h2>
           <div className="w-full flex gap-4 flex-wrap">
             {cropData.map((crop, index) => (
               <CropCard
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,32 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      accent: "#70e000",
-      link: "#004bff",
-      primary: "#F4F4F4",
-      white: "#ffffff",
-      bred: "#d90429",
-    },
-    boxShadow: {
-      box: "10px 10px 40px #00000041, inset -10px -10px 40px #ffffff40",
-    },
-    fontFamily: {
-      Poppins: ["Poppins", "sans-serif"],
-    },
     extend: {
+      colors: {
+        accent: "#70e000",
+        link: "#004bff",
+        primary: "#F4F4F4",
+        white: "#ffffff",
+        bred: "#d90429",
+        // Extend the default green color palette
+        green: {
+          50: '#f0fdf4',  // Light green
+          100: '#dcfce7',
+          200: '#bbf7d0',
+          300: '#86efac',
+          400: '#4ade80',
+          500: '#22c55e',
+          600: '#16a34a',
+          700: '#15803d',
+          800: '#166534',
+          900: '#14532d',
+        },
+      },
+      boxShadow: {
+        box: "10px 10px 40px #00000041, inset -10px -10px 40px #ffffff40",
+      },
       fontFamily: {
+        Poppins: ["Poppins", "sans-serif"],
         Sat: ["Satisfy", "sans-serif"],
       },
     },
